b96e4422a9b090025332fce18e8324cebd6b49a5
Mike Cohen <mjoecohen@gmail.com>
2019-08-08 18:01:39 +0100

    staging: setup staging / production environments

--- src/main/java/org/brightblock/gaia/api/GaiaHubController.java
+++ src/main/java/org/brightblock/gaia/api/GaiaHubController.java
@@ -37,7 +37,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 @Controller
-@CrossOrigin(origins = { "http://localhost:8080", "https://radicle.art", "https://dbid.io", "https://brightblock.org", "http://localhost:8888" }, maxAge = 6000)
+@CrossOrigin(origins = { "http://localhost:8080", "http://localhost:8081", "https://radicle.art", "https://tart.radiclesociety.org", "https://tdbid.radiclesociety.org",  "https://dbid.io", "https://brightblock.org", "http://localhost:8888" }, maxAge = 6000)
 /**
  * @see https://github.com/blockstack/gaia
  * @author mike cohen
